b8942bed6b38c007da6c84e91c1d7f92d16d88b2
Paul Grever <paulgrever@Pauls-MacBook-Pro-2.local>
2016-03-18 11:18:23 -0600

    added basic folder structure

---
+++ package.js
@@ -0,0 +1,6 @@
+Package.describe({
+  summary: 'Import Products from a CSV',
+  name: 'getoutfitted:reaction-product-importer',
+  version: '0.1.0',
+  git: 'https://github.com/getoutfitted/reaction-product-importer'
+});
